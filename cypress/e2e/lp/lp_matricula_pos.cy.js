f3f6ed3c4bc3f126646736b8a213b057609c1480
Gabriel Esquilage <roccaesquil19910@gmail.com>
2025-12-01 17:00:33 -0300

    finalização_matricula_pos

--- cypress/e2e/lp/lp_matricula_pos.cy.js
+++ cypress/e2e/lp/lp_matricula_pos.cy.js
@@ -41,9 +41,9 @@ describe('Valida LP Pós-Graduação', () => {
 
         cy.contains('button', 'Próximo').scrollIntoView().click()
 
-
         cy.contains('Digite seu CEP').scrollIntoView().parent()
             .find('input, textarea').clear().type('87020-015')
+
         cy.wait(1000)
 
         cy.contains('Número').scrollIntoView().parent()
@@ -52,29 +52,35 @@ describe('Valida LP Pós-Graduação', () => {
         cy.contains('Button', 'Próximo').scrollIntoView().click()
 
         cy.get('button[role="combobox"]')
-          .contains('Selecione um curso')
-          .click();
+            .contains('Selecione um curso')
+            .click();
+
         cy.contains('ESPECIALIZAÇÃO EM ARQUITETURA E URBANISMO 123')
-          .should('be.visible')
-          .click();
+            .should('be.visible')
+            .click();
 
         cy.selecionarOpcaoAleatoriaDireto('Estado')
         cy.selecionarOpcaoAleatoriaDireto('Cidade')
         cy.selecionarOpcaoAleatoriaDireto('Qual polo prefere estudar?')
         cy.selecionarOpcaoAleatoriaDireto('Planos de Pagamento')
         cy.selecionarOpcaoAleatoriaDireto('Dia de vencimento das mensalidades')
+
         cy.contains('label', 'Pix').click()
+
         cy.contains('Possui cupom de desconto?').scrollIntoView().parent()
             .find('input, textarea').clear().type('bolsa100')
 
-        cy.get('[role="checkbox"][data-slot="checkbox"]').eq(0).click();
-        cy.get('[role="checkbox"][data-slot="checkbox"]').eq(1).click();
+        cy.get('[role="checkbox"][data-slot="checkbox"]').eq(0).click()
+        cy.get('[role="checkbox"][data-slot="checkbox"]').eq(1).click()
 
-        cy.contains('p', 'Pagamento isento com cupom de 100% de desconto!').should('be.visible')
+        cy.contains('p', 'Pagamento isento com cupom de 100% de desconto!')
+            .should('be.visible')
 
         cy.contains('Button', 'Enviar').scrollIntoView().click()
-        cy.contains('button', 'Fechar', { timeout: 60000 }).should('be.visible').and('be.enabled')
 
+        cy.contains('button', 'Fechar', { timeout: 60000 })
+            .should('be.visible')
+            .and('be.enabled')
     })
 
     it('Deve fazer o cadastro completo - Sem Cupom - PIX', () => {
@@ -115,9 +121,9 @@ describe('Valida LP Pós-Graduação', () => {
 
         cy.contains('button', 'Próximo').scrollIntoView().click()
 
-
         cy.contains('Digite seu CEP').scrollIntoView().parent()
             .find('input, textarea').clear().type('87020-015')
+
         cy.wait(1000)
 
         cy.contains('Número').scrollIntoView().parent()
@@ -126,28 +132,29 @@ describe('Valida LP Pós-Graduação', () => {
         cy.contains('Button', 'Próximo').scrollIntoView().click()
 
         cy.get('button[role="combobox"]')
-          .contains('Selecione um curso')
-          .click();
+            .contains('Selecione um curso')
+            .click();
+
         cy.contains('ESPECIALIZAÇÃO EM ARQUITETURA E URBANISMO 123')
-          .should('be.visible')
-          .click();
+            .should('be.visible')
+            .click();
 
         cy.selecionarOpcaoAleatoriaDireto('Estado')
         cy.selecionarOpcaoAleatoriaDireto('Cidade')
         cy.selecionarOpcaoAleatoriaDireto('Qual polo prefere estudar?')
         cy.selecionarOpcaoAleatoriaDireto('Planos de Pagamento')
         cy.selecionarOpcaoAleatoriaDireto('Dia de vencimento das mensalidades')
+
         cy.contains('label', 'Pix').click()
-        //cy.contains('Possui cupom de desconto?').scrollIntoView().parent()
-            //.find('input, textarea').clear().type('bolsa100')
 
-        cy.get('[role="checkbox"][data-slot="checkbox"]').eq(0).click();
-        cy.get('[role="checkbox"][data-slot="checkbox"]').eq(1).click();
-
+        cy.get('[role="checkbox"][data-slot="checkbox"]').eq(0).click()
+        cy.get('[role="checkbox"][data-slot="checkbox"]').eq(1).click()
 
         cy.contains('Button', 'Enviar').scrollIntoView().click()
-        cy.contains('button', 'Fechar', { timeout: 60000 }).should('be.visible').and('be.enabled')
 
+        cy.contains('button', 'Fechar', { timeout: 60000 })
+            .should('be.visible')
+            .and('be.enabled')
     })
 
     it('Deve fazer o cadastro completo - Sem Cupom - cartão de crédito', () => {
@@ -188,9 +195,9 @@ describe('Valida LP Pós-Graduação', () => {
 
         cy.contains('button', 'Próximo').scrollIntoView().click()
 
-
         cy.contains('Digite seu CEP').scrollIntoView().parent()
             .find('input, textarea').clear().type('87020-015')
+
         cy.wait(1000)
 
         cy.contains('Número').scrollIntoView().parent()
@@ -199,45 +206,49 @@ describe('Valida LP Pós-Graduação', () => {
         cy.contains('Button', 'Próximo').scrollIntoView().click()
 
         cy.get('button[role="combobox"]')
-          .contains('Selecione um curso')
-          .click();
+            .contains('Selecione um curso')
+            .click();
+
         cy.contains('ESPECIALIZAÇÃO EM ARQUITETURA E URBANISMO 123')
-          .should('be.visible')
-          .click();
+            .should('be.visible')
+            .click();
 
         cy.selecionarOpcaoAleatoriaDireto('Estado')
         cy.selecionarOpcaoAleatoriaDireto('Cidade')
         cy.selecionarOpcaoAleatoriaDireto('Qual polo prefere estudar?')
         cy.selecionarOpcaoAleatoriaDireto('Planos de Pagamento')
         cy.selecionarOpcaoAleatoriaDireto('Dia de vencimento das mensalidades')
-        cy.contains('label', 'Cartão de Crédito').click()
-        //cy.contains('Possui cupom de desconto?').scrollIntoView().parent()
-        //e    .find('input, textarea').clear().type('bolsa100')
 
-        cy.get('[role="checkbox"][data-slot="checkbox"]').eq(0).click();
-        cy.get('[role="checkbox"][data-slot="checkbox"]').eq(1).click();
+        cy.contains('label', 'Cartão de Crédito').click()
+
+        cy.get('[role="checkbox"][data-slot="checkbox"]').eq(0).click()
+        cy.get('[role="checkbox"][data-slot="checkbox"]').eq(1).click()
 
         cy.contains('Número do cartão').scrollIntoView().parent()
             .find('input').clear().type('4000000000000010')
+
         cy.contains('Nome que está no cartão').scrollIntoView().parent()
             .find('input').clear().type('TESTE TESTE')
+
         cy.contains('Mês de Vencimento ').scrollIntoView().parent()
             .find('input').clear().type('10')
+
         cy.contains('Ano de Vencimento').scrollIntoView().parent()
             .find('input').clear().type('2029')
+
         cy.contains('CVV').scrollIntoView().parent()
             .find('input').clear().type('129')
-        
+
         cy.wait(1500)
 
         cy.contains('Button', 'Enviar').scrollIntoView().click()
-        
-        cy.contains('button', 'Fechar', { timeout: 60000 }).should('be.visible').and('be.enabled')
-        
 
+        cy.contains('button', 'Fechar', { timeout: 60000 })
+            .should('be.visible')
+            .and('be.enabled')
     })
 
-    it('Deve retornar erro no cadastro - teste com mesmos dados de Nome, Email, CPF, Curso', () => { 
+    it('Deve retornar erro no cadastro - teste com mesmos dados de Nome, Email, CPF, Curso', () => {
 
         cy.visit('https://matriculapos.dev.fatecie.edu.br/inscription')
 
@@ -245,7 +256,7 @@ describe('Valida LP Pós-Graduação', () => {
         cy.contains('Nome Completo').scrollIntoView().parent()
             .find('input, textarea').clear().type(sharedName)
 
-        cy.logToTerminal(`Usando Nome salvo: ${sharedCpf}`)    
+        cy.logToTerminal(`Usando Nome salvo: ${sharedCpf}`)
         cy.contains('CPF').scrollIntoView().parent()
             .find('input, textarea').clear().type(sharedCpf)
 
@@ -266,9 +277,9 @@ describe('Valida LP Pós-Graduação', () => {
 
         cy.contains('button', 'Próximo').scrollIntoView().click()
 
-
         cy.contains('Digite seu CEP').scrollIntoView().parent()
             .find('input, textarea').clear().type('87080-400')
+
         cy.wait(6000)
 
         cy.contains('Número').scrollIntoView().parent()
@@ -277,28 +288,50 @@ describe('Valida LP Pós-Graduação', () => {
         cy.contains('Button', 'Próximo').scrollIntoView().click()
 
         cy.get('button[role="combobox"]')
-          .contains('Selecione um curso')
-          .click();
+            .contains('Selecione um curso')
+            .click();
+
         cy.contains('ESPECIALIZAÇÃO EM ARQUITETURA E URBANISMO 123')
-          .should('be.visible')
-          .click();
+            .should('be.visible')
+            .click();
 
         cy.selecionarOpcaoAleatoriaDireto('Estado')
         cy.selecionarOpcaoAleatoriaDireto('Cidade')
         cy.selecionarOpcaoAleatoriaDireto('Qual polo prefere estudar?')
         cy.selecionarOpcaoAleatoriaDireto('Planos de Pagamento')
         cy.selecionarOpcaoAleatoriaDireto('Dia de vencimento das mensalidades')
-        cy.contains('label', 'Pix').click()
-        //cy.contains('Possui cupom de desconto?').scrollIntoView().parent()
-            //.find('input, textarea').clear().type('bolsa100')
 
-        cy.get('[role="checkbox"][data-slot="checkbox"]').eq(0).click();
-        cy.get('[role="checkbox"][data-slot="checkbox"]').eq(1).click();
+        cy.contains('label', 'Pix').click()
+
+        cy.get('[role="checkbox"][data-slot="checkbox"]').eq(0).click()
+        cy.get('[role="checkbox"][data-slot="checkbox"]').eq(1).click()
 
         cy.wait(6000)
+
         cy.contains('Button', 'Enviar').scrollIntoView().click()
+
         cy.wait(1500)
 
-        cy.contains('button', 'Fechar', { timeout: 60000 }).should('be.visible').and('be.enabled')
+        cy.contains('button', 'Fechar', { timeout: 60000 })
+            .should('be.visible')
+            .and('be.enabled')
+            .click()
+    })
+
+    it('Deve retornar erro ao passar step 1 para 2, sem preenchimento', () => {
+
+        cy.visit('https://matriculapos.dev.fatecie.edu.br/inscription')
+
+        cy.contains('button', 'Próximo').scrollIntoView().click()
+
+        cy.get('span.text-red-500')
+            .then($spans => {
+                const textos = [...$spans].map(s => s.innerText.trim())
+                expect(textos).to.include.members([
+                    'Campo Obrigatório',
+                    'E-mail inválido',
+                    'Por Favor, selecione uma data'
+                ])
+            })
     })
 })
